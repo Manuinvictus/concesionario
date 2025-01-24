09e86928b5d0389ca09f0a16860f6fbe36aae6cb
Gorilo <mistergorilo@gmail.com>
2025-01-24 20:23:10 +0100

    Terminados html 1.0

--- Concesionario/src/main/java/com/salesianos/concesionario/model/CochePorMarca.java
+++ Concesionario/src/main/java/com/salesianos/concesionario/model/CochePorMarca.java
@@ -9,14 +9,25 @@ public class CochePorMarca {
 	@Id
 	private long idcoche;
 	private String modelo;
-	private int idmarca;
-
+	private String nombremarca;
+    private int anyofabricacion;
+    private int precio;
+    private String moneda;
+    private String nombre;
+    private String apellidos;
+    private String cargo;
 	
 	//Builder
-	public CochePorMarca(long idcoche, String modelo, int idmarca) {
+	public CochePorMarca(long idcoche, String modelo, String nombremarca, int anyofabricacion, int precio, String moneda, String nombre, String apellidos, String cargo) {
 		this.idcoche = idcoche;
 		this.modelo = modelo;
-		this.idmarca = idmarca;
+		this.nombremarca = nombremarca;
+		this.anyofabricacion = anyofabricacion;
+		this.precio = precio;
+		this.moneda = moneda;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.cargo = cargo;
 	}
 
 
@@ -25,30 +36,73 @@ public class CochePorMarca {
 		return idcoche;
 	}
 
-
 	public String getModelo() {
 		return modelo;
 	}
 
-
-	public int getIdmarca() {
-		return idmarca;
+	public String getNombremarca() {
+		return nombremarca;
 	}
 
+	public int getAnyofabricacion() {
+		return anyofabricacion;
+	}
 
+	public int getPrecio() {
+		return precio;
+	}
+
+	public String getMoneda() {
+		return moneda;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	//Setter
 	public void setIdcoche(long idcoche) {
 		this.idcoche = idcoche;
 	}
 
-
-	//Setter
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
 
+	public void setNombremarca(String nombremarca) {
+		this.nombremarca = nombremarca;
+	}
 
-	public void setIdmarca(int idmarca) {
-		this.idmarca = idmarca;
+	public void setAnyofabricacion(int anyofabricacion) {
+		this.anyofabricacion = anyofabricacion;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public void setMoneda(String moneda) {
+		this.moneda = moneda;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
 	}
 
 	
